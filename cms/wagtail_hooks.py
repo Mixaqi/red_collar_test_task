f47e622bd084a6e97d599cf1fbcb2e9f33f5146c
Mikhail Khvalko <mikhailkhvalko@outlook.com>
2026-02-19 11:35:37 +0300

    feat: russian lang in wagtail admin

--- cms/wagtail_hooks.py
+++ cms/wagtail_hooks.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
@@ -9,21 +10,30 @@ from geopoints.models import MapPoint, Message
 class MapPointViewSet(SnippetViewSet):
     model = MapPoint
     list_display = ["location", "user", "created_at"]
+    list_display_labels = {
+        "location": _("Локация"),
+        "user": _("Пользователь"),
+        "created_at": _("Дата создания"),
+    }
 
     panels: list[LeafletPanel | FieldPanel] = [
-        LeafletPanel("location"),
-        FieldPanel("user"),
+        LeafletPanel("location", heading=_("Точка")),
+        FieldPanel("user", heading=_("Пользователь")),
     ]
 
 
 class MessageViewSet(SnippetViewSet):
     model = Message
     list_display = ["text", "point", "user"]
-
+    list_display_labels = {
+        "text": _("Текст"),
+        "user": _("Пользователь"),
+        "created_at": _("Дата создания"),
+    }
     panels: list[FieldPanel] = [
-        FieldPanel("point"),
-        FieldPanel("text"),
-        FieldPanel("user"),
+        FieldPanel("point", heading=_("Точка")),
+        FieldPanel("text", heading=_("Текст")),
+        FieldPanel("user", heading=_("Пользователь")),
     ]
 
 
--- geopoints/models.py
+++ geopoints/models.py
@@ -12,23 +12,41 @@ from authentication.models import User
 
 
 class MapPoint(Model):
-    location = PointField(srid=4326, unique=True)
-    user = ForeignKey(User, on_delete=SET_NULL, null=True, blank=True)
-    created_at: DateTimeField = DateTimeField(auto_now_add=True)
+    location = PointField(srid=4326, unique=True, verbose_name=("Локация"))
+    user = ForeignKey(
+        User, on_delete=SET_NULL, null=True, blank=True, verbose_name="Пользователь"
+    )
+    created_at: DateTimeField = DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания"
+    )
+
+    class Meta:
+        verbose_name = "Точка на карте"
+        verbose_name_plural = "Точки на карте"
 
     def __str__(self) -> str:
-        return f"Point #{self.pk} [{self.location.y:.4f}, {self.location.x:.4f}]"
+        return f"Точка #[{self.location.y:.4f}, {self.location.x:.4f}]"
 
 
 class Message(Model):
-    point = ForeignKey(MapPoint, on_delete=CASCADE, related_name="messages")
-    text: TextField = TextField()
-    user = ForeignKey(User, on_delete=SET_NULL, null=True, blank=True)
-    created_at: DateTimeField = DateTimeField(auto_now_add=True)
+    point = ForeignKey(
+        MapPoint, on_delete=CASCADE, related_name="messages", verbose_name="Точка"
+    )
+    text: TextField = TextField(verbose_name="Текст")
+    user = ForeignKey(
+        User, on_delete=SET_NULL, null=True, blank=True, verbose_name="Пользователь"
+    )
+    created_at: DateTimeField = DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания"
+    )
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
 
     def __str__(self) -> str:
         if self.user:
             username = self.user.username
         else:
-            username = "Deleted User"
-        return f"By {username} for point #{self.point.pk}: {self.text[:20]}..."
+            username = "Удаленный пользователь"
+        return f"От пользователя {username} для точки #{self.point.pk}: {self.text[:20]}..."
