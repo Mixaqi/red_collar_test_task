0af2e8537fc952b2fd3f333bcdb1b1eb9e53100e
Mikhail Khvalko <mikhailkhvalko@outlook.com>
2026-02-23 12:39:08 +0300

    wip: CustomForm class for point creating

--- cms/snippets.py
+++ cms/snippets.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from django.forms import ModelForm
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.views.snippets import SnippetViewSet
 from wagtailgeowidget.panels import LeafletPanel
@@ -12,8 +15,19 @@ class MapPointViewSet(SnippetViewSet):
 
     panels = [
         LeafletPanel("location", heading="Точка"),
-        FieldPanel("user", heading="Пользователь", read_only=True),
+        FieldPanel("user", heading="Пользователь"),
     ]
+
+    def get_form_class(self, for_update: bool = False) -> type[ModelForm]:
+        form_class: type[ModelForm] = super().get_form_class(for_update=for_update)
+
+        class CustomForm(form_class):  # type: ignore[misc, valid-type]
+            def __init__(self, *args: Any, **kwargs: Any) -> None:
+                super().__init__(*args, **kwargs)
+                if self.instance and self.instance.pk:
+                    self.fields["user"].disabled = True
+
+        return CustomForm
 
 
 class MessageViewSet(SnippetViewSet):
